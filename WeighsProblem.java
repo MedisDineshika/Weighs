83099882fd981b07f9d4f9d8b1629ca369eb8a5f
WDM <52590387+MedisDineshika@users.noreply.github.com>
2020-01-19 17:02:52 +0530

    Add files via upload

---
+++ WeighsProblem.java
@@ -0,0 +1,26 @@
+
+
+
+public class WeighsProblem{
+  
+   public static void main(String[] args) {
+    for (int i =1; i < 40 ; i *=2 ) {
+        System.out.println("Weigh " + i + "kg");
+        
+      }
+      
+
+}
+ 
+}
+
+
+
+
+
+
+
+
+
+
+
